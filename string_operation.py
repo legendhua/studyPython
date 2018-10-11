545a118b47233bb56189623a52f19e2d0e917dab
legendhua <zhangguanghualgd@163.com>
2018-10-11 22:53:54 +0800

    add more string operations

--- string_operation.py
+++ string_operation.py
@@ -30,3 +30,33 @@ print(line.startswith('I'))   # False
 # 判断某子字符串是否存在
 print('you' in line)  # 返回bool型数据
 print(line.find('you'))  # 返回子串存在的首个位置下标，20
+
+# 格式化操作
+x = 1234
+# 第一个为默认正常输出，第二个为6位左对齐输出，第三个为6位右对齐输出，第四个为6位右对齐左补0
+res = "the result is:...%d...%-6d...%6d...%06d"%(x,x,x,x)
+print(res)
+
+y = 1.23456
+# 第一个为6位左对齐3位小数，第二个为6位右对齐左补03位小数
+res = "the result is:...%-6.3f...%06.3f..."%(y,y)
+print(res)
+
+# 基于字典的字符串格式化
+reply = '''
+Greeting...
+Hello,%(name)s,
+my age is %(age)d.
+'''
+values = {'name':'Bob','age':30}
+print(reply % values)
+
+# 通过位置或者关键字来进行格式化字符串
+template = '{0}, {1} and {2}'   # by position
+print(template.format('spam','ham','eggs'))
+
+template = '{motto}, {pork} and {food}'  # by keyword
+print(template.format(motto='spam',pork='ham',food='eggs'))
+
+template = '{motto}, {0} and {food}'  # by both
+print(template.format('ham',motto='spam', food = 'eggs'))
